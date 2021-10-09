b3f9a12d60a1b0374db0e1334337f0baaee5e754
daproo12 <71934807+daproo12@users.noreply.github.com>
2021-10-09 20:53:26 +0700

    Add files via upload

---
+++ public/assets/js/init-alpine.js
@@ -0,0 +1,80 @@
+function data() {
+  function getThemeFromLocalStorage() {
+    // if user already changed the theme, use it
+    if (window.localStorage.getItem('dark')) {
+      return JSON.parse(window.localStorage.getItem('dark'))
+    }
+
+    // else return their preferences
+    return (
+      !!window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    )
+  }
+
+  function setThemeToLocalStorage(value) {
+    window.localStorage.setItem('dark', value)
+  }
+
+  return {
+    dark: getThemeFromLocalStorage(),
+    toggleTheme() {
+      this.dark = !this.dark
+      setThemeToLocalStorage(this.dark)
+    },
+    isSideMenuOpen: false,
+    toggleSideMenu() {
+      this.isSideMenuOpen = !this.isSideMenuOpen
+    },
+    closeSideMenu() {
+      this.isSideMenuOpen = false
+    },
+    isNotificationsMenuOpen: false,
+    toggleNotificationsMenu() {
+      this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen
+    },
+    closeNotificationsMenu() {
+      this.isNotificationsMenuOpen = false
+    },
+    isProfileMenuOpen: false,
+    toggleProfileMenu() {
+      this.isProfileMenuOpen = !this.isProfileMenuOpen
+    },
+    closeProfileMenu() {
+      this.isProfileMenuOpen = false
+    },
+    isVerifikasiOpen: false,
+    toggleVerifikasiMenu() {
+      this.isVerifikasiOpen = !this.isVerifikasiOpen
+    },
+    closeVerifikasi() {
+      this.isVerifikasiOpen = false
+    },
+    isPagesMenuOpen: false,
+    togglePagesMenu() {
+      this.isPagesMenuOpen = !this.isPagesMenuOpen
+    },
+    // Modal MOU
+    isModalMOUOpen: false,
+    trapCleanup: null,
+    openModalMOU() {
+      this.isModalMOUOpen = true
+      this.trapCleanup = focusTrap(document.querySelector('#modalMOU'))
+    },
+    closeModalMOU() {
+      this.isModalMOUOpen = false
+      this.trapCleanup()
+    },
+    // Modal Tambah Keterangan Kerjasama
+    isModalKeteranganKerjasamaOpen: false,
+    trapCleanup: null,
+    openModalKeteranganKerjasama() {
+      this.isModalKeteranganKerjasamaOpen = true
+      this.trapCleanup = focusTrap(document.querySelector('#modalMOU'))
+    },
+    closeModalKeteranganKerjasama() {
+      this.isModalKeteranganKerjasamaOpen = false
+      this.trapCleanup()
+    },
+  }
+}
